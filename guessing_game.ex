59dca8d78046bee7e4f59e80dc9920237f97ec45
“Nidhi <nidhi.sarvaiya@icicletech.com>
2019-11-02 23:17:40 -0400

    Added stategy to solve problem

---
+++ guessing_game.ex
@@ -0,0 +1,9 @@
+defmodule GuessingGame do
+#strategy to guess number
+# Guess number between range of higher and lower number -> Guess middle number
+# tell user our guess
+# "Yes" -> Correct
+# "Bigger number" -> Call Bigger(high,low) function
+# "Smaller number" -> Call Smaller(high,low) function
+# Anything else -> Throw valid exception message to input valid response
+end
