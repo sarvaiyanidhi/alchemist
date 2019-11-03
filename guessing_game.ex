2ecafb2eb5f096a4f5f81ef0a57b8d8a99398d92
“Nidhi <nidhi.sarvaiya@icicletech.com>
2019-11-03 00:12:11 -0400

    Added logic to guess number

--- guessing_game.ex
+++ guessing_game.ex
@@ -9,9 +9,38 @@ defmodule GuessingGame do
   # "Smaller number" -> Call Smaller(high,low) function
   # Anything else -> Throw valid exception message to input valid response
 
+
+  def guess(low,high) when low > high, do: guess(high,low)
+
+  def guess(low,high) do
+    answer = IO.gets("May be you are thinking of #{mid(low,high)}\n")
+
+    case String.trim(answer) do
+      "bigger" ->
+        bigger(low,high)
+      "smaller" ->
+        smaller(low,high)
+      "yes" ->
+        IO.puts("I guessed correct number")
+      _ ->
+        IO.puts(~s(Please enter "bigger", "smaller" or "yes" ))
+        guess(low,high)
+    end
+  end
+
   #Function to get middle number
   def mid(low, high) do
     div(low + high, 2)
+  end
+
+  def bigger(low,high) do
+    new_low = min(high,mid(low,high) + 1)
+    guess(new_low,high)
+  end
+
+  def smaller(low,high) do
+    new_high = max(low,mid(low,high) - 1)
+    guess(low,new_high)
   end
 
 end
