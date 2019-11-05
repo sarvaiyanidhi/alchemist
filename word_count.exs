9a277027e64b9243de5eb3e1f0a78416560448b8
“Nidhi <nidhi.sarvaiya@icicletech.com>
2019-11-04 22:26:04 -0500

    Added filter to remove empty space

--- word_count.exs
+++ word_count.exs
@@ -1,4 +1,4 @@
 filename = IO.gets("File to count word from: ") |> String.trim
-words = File.read!(filename) |> String.split(~r{(\\n|[^\w']+)})
+words = File.read!(filename) |> String.split(~r{(\\n|[^\w']+)}) |> Enum.filter(fn x -> x != "" end)
 IO.inspect words
 words |> Enum.count |> IO.puts
